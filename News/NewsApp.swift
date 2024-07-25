cbf6b4e230a4097191a4c908778f0eb34e4a17f2
Ivan Putra Pratama <ifam4@yahoo.com>
2024-07-25 08:33:24 +0700

    Initial Commit

---
+++ News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by Helena on 25/07/24.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
